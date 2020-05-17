b060742c964c12eea965712c953982c857d813a5
marshall942 <isabel.pitcher@yahoo.com>
2020-05-18 10:41:08 +1200

    d

--- week5/class1.php
+++ week5/class1.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="css/style.css">
 <?php
 
 echo "Hello world"; //echo means print
@@ -7,3 +8,9 @@ echo "<h1>Hello world</h1>";
 echo "<p>Hello world</p>";
 
 echo "<p style='color: red'>Hello world</p>"
+
+$i = 0; //set counter
+while ($i < 100){ //set condition
+    echo "Hello"; //do something if condition is true
+    $i = $i + 1; //update counter
+}
